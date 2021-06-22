76a47e7c2efa67e87acae2486ae9e585fccacf88
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-06-22 21:29:08 +0800

    Add seeder

--- seed-run.js
+++ seed-run.js
@@ -1,16 +1,73 @@
-const { seedRun } = require('./libs');
+const chalk = require('chalk');
+const path = require('path');
+const fastGlob = require('fast-glob');
+const Parse = require('parse/node');
+const {
+  isRequiredEnvironmentAvailable, cloading, cright, cerror, csuccess,
+} = require('./libs/helpers');
+const { buildInfo, seederDirectory } = require('./libs/system');
 
-exports.command = 'seed';
+const {
+  APPLICATION_ID, MASTER_KEY, SERVER_URL,
+} = process.env;
 
-exports.aliases = ['seed-run'];
+const command = 'seed';
 
-exports.describe = 'Seed data';
+// const aliases = ['seed:run'];
 
-// exports.builder = {};
+const describe = 'Seed data';
 
-exports.handler = (args) => {
-  console.log('Migration up');
-  seedRun();
+// const builder = {};
 
-  // throw new Error('Not implement yet!');
+/**
+ * Seed data
+ */
+async function seedRun() {
+  await isRequiredEnvironmentAvailable(SERVER_URL, APPLICATION_ID, MASTER_KEY);
+  const seederFiles = fastGlob.sync(`${path.resolve(process.cwd(), seederDirectory)}/*.js`);
+
+  const sortedFiles = seederFiles.sort(
+    (a, b) => a.toLowerCase().localeCompare(b.toLocaleLowerCase()),
+  );
+
+  console.log('Run all files inside databases/seeders');
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const filepath of sortedFiles) {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    const seederScript = require(filepath);
+    const filename = path.basename(filepath);
+
+    console.log(cloading(`Seeding ${filename}`));
+
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      await seederScript.run(Parse);
+      console.log(cright(`Done    ${filename}\n`));
+    } catch (error) {
+      console.log(cerror(error.message));
+      // Break the loop when error happen
+      break;
+    }
+
+    // Add timeout to safety finish one function before going to next
+  }
+}
+
+// eslint-disable-next-line no-unused-vars
+const handler = async (args) => {
+  console.log(`\n${buildInfo}\n`);
+
+  console.log(chalk`Run seeders on parse-server at {underline ${SERVER_URL}}\n`);
+
+  await seedRun();
+
+  console.log(`\n${csuccess('Successfully run seeders.\n')}`);
+};
+
+module.exports = {
+  command,
+  describe,
+  // builder,
+  handler,
 };
