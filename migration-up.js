b076a2f779651cb39120c9b0911421b5c68014d5
Fakhrullah Padzil <fakhrullah@fajarhac.com>
2021-06-22 12:59:51 +0800

    Refactor migration
    Use reusable functions.
    Handle error for partially success migrations.

--- migration-up.js
+++ migration-up.js
@@ -1,18 +1,23 @@
 require('dotenv').config();
 const path = require('path');
 const chalk = require('chalk');
-const fastGlob = require('fast-glob');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const Parse = require('parse/node');
+
 const MigrationError = require('./libs/MigrationError');
-const { migrationDirectory, buildInfo } = require('./libs/system');
+const { buildInfo } = require('./libs/system');
 const {
-  cerror, csuccess, cright, cloading,
+  cerror, csuccess, cright, cloading, isRequiredEnvironmentAvailable,
 } = require('./libs/helpers');
+const { getAllMigrations, saveAllMigrations } = require('./libs/migration-model');
 
 const {
   APPLICATION_ID, JAVASCRIPT_KEY, MASTER_KEY, SERVER_URL,
 } = process.env;
+
+/**
+ * @typedef {import('./libs/migration-model').MigrationDetail} MigrationDetail}
+ */
 
 exports.command = 'migrate';
 
@@ -27,165 +32,81 @@ exports.describe = 'Run migrations.';
 //     example: '',
 //   });
 
-/**
- *
- * @returns {string}
- */
-function isRequiredEnvironmentAvailable() {
-  if (APPLICATION_ID === undefined) {
-    return new Error('APPLICATION_ID environment is required');
-  }
-  if (MASTER_KEY === undefined) {
-    return new Error('MASTER_KEY environment is required');
-  }
-  if (SERVER_URL === undefined) {
-    return new Error('SERVER_URL environment is required');
-  }
-  return true;
-}
-
 Parse.initialize(APPLICATION_ID, JAVASCRIPT_KEY, MASTER_KEY);
 Parse.serverURL = SERVER_URL;
 
 /**
- * @param {String[]} migrationsDone
- */
-const recordMigration = async (migrationsDone) => {
-  const migrations = migrationsDone.map((migrationDone) => {
-    const MigrationObject = Parse.Object.extend('Migration');
-    const migrationObject = new MigrationObject();
-    migrationObject.set({
-      name: migrationDone,
-    });
-    return migrationObject;
-  });
-
-  return Parse.Object.saveAll(migrations, { useMasterKey: true });
-};
-
-/**
- * @returns {Promise<string[]>}
- */
-const readMigration = async () => {
-  const migrationQuery = new Parse.Query('Migration');
-  const migrations = await migrationQuery.findAll({ useMasterKey: true });
-  return migrations.map((migration) => migration.get('name'));
-};
-
-/**
  *
- * @param {boolean} [isSeedRun]
+ * @returns {Promise<MigrationDetail[]>}
  */
-async function migrationUp(isSeedRun) {
-  const isAllRequiredEnv = isRequiredEnvironmentAvailable();
+async function migrationUp() {
+  // Check required environment and stop function when not environment is not enough
+  const checkToConnectToParseServer = await isRequiredEnvironmentAvailable(
+    SERVER_URL,
+    APPLICATION_ID,
+    MASTER_KEY,
+  );
+  if (!checkToConnectToParseServer) {
+    throw checkToConnectToParseServer;
+  }
 
-  /** @type {String[]} */
+  /** @type {MigrationDetail[]} */
   const migrationsDone = [];
 
-  let isMigrationSuccess = false;
+  /** @type {MigrationDetail[]} */
+  const migrationsToRun = await getAllMigrations(Parse)
+    .then((migrations) => migrations.filter((migration) => migration.status === 'down'));
 
-  if (isAllRequiredEnv !== true) {
-    throw isAllRequiredEnv;
-  }
-
-  const migrationFiles = fastGlob.sync(`${path.resolve(process.cwd(), migrationDirectory)}/**`);
-
-  if (migrationFiles.length === 0) {
-    throw new Error('No migration files is found.');
-  }
-
-  const sortedFiles = migrationFiles.sort(
-    (a, b) => a.toLowerCase().localeCompare(b.toLocaleLowerCase()),
-  );
-
-  const alreadyRunMigrations = await readMigration();
-
-  const migrationFilesToRun = sortedFiles.filter(
-    (filepath) => !alreadyRunMigrations.includes(path.basename(filepath)),
-  );
-
-  if (migrationFilesToRun.length === 0) {
+  if (migrationsToRun.length === 0) {
     throw new MigrationError('No migrations were executed, database schema was already up to date.');
   }
 
   // run up() of all files
   // eslint-disable-next-line no-restricted-syntax
-  for (const filepath of migrationFilesToRun) {
+  for (const migrationToRun of migrationsToRun) {
     // eslint-disable-next-line import/no-dynamic-require, global-require
-    const migrationScript = require(filepath);
-    const migrationFilename = path.basename(filepath);
+    const migrationScript = require(migrationToRun.fullpath);
+    const migrationFilename = path.basename(migrationToRun.name);
 
     console.log(cloading(`Migrating ${migrationFilename}`));
 
-    // eslint-disable-next-line no-await-in-loop
-    const migration = await migrationScript.up(Parse);
-
-    if (migration) {
+    try {
+      // eslint-disable-next-line no-await-in-loop
+      await migrationScript.up(Parse);
       console.log(cright(`Migrated  ${migrationFilename}\n`));
-      migrationsDone.push(migrationFilename);
-    } else {
-      // throw new Error(migration);
-      isMigrationSuccess = false;
+      migrationsDone.push(migrationToRun);
+    } catch (error) {
+      console.log(cerror(error.message));
       // Break the loop when error happen
       break;
     }
-
-    isMigrationSuccess = true;
 
     // Add timeout to safety finish one function before going to next
     // eslint-disable-next-line no-await-in-loop
     await new Promise((resolve) => setTimeout(resolve, 500));
   }
 
-  // Record ran migration files
-  await recordMigration(migrationsDone);
-
-  //
-  if (!isMigrationSuccess) {
-    throw new Error('Unexpected error. What is it? It is unexpected. No one knows.');
-  }
-
-  // Is all migrations is ran?
-
-  // TODO: Seed
-  // if (isSeedRun) {
-  //   // eslint-disable-next-line no-use-before-define
-  //   seedRun();
-  // }
+  return migrationsDone;
 }
 
-exports.handler = (args) => {
+exports.handler = async (args) => {
+  /** @type {boolean} */
+  const shouldRunSeed = args.seed || false;
+
   console.log(`\n${buildInfo}\n`);
-
-  // eslint-disable-next-line global-require
-
-  // Validate required enviroment
-  if (!APPLICATION_ID || !MASTER_KEY || !SERVER_URL) {
-    console.log(cerror('Detail to connect to server is not enough.\n'));
-    console.log(
-      'These environment is required to connect to your parse-server: \n\n',
-      `process.env.SERVER_URL     \t: ${SERVER_URL} \n`,
-      `process.env.APPLICATION_ID \t: ${APPLICATION_ID} \n`,
-      `process.env.MASTER_KEY     \t: ${MASTER_KEY} \n`,
-    );
-    return;
-  }
 
   // Validate parse-server connection
 
   console.log(chalk`Run migration on parse-server at {underline ${SERVER_URL}}\n`);
 
-  migrationUp(false)
-    .then(() => {
-      console.log(`\n${csuccess('Successfully run migrations.\n')}`);
-    })
-    .catch((err) => {
-      if (err instanceof MigrationError) {
-        console.log(`${err.message}\n`);
-      } else {
-        console.log(`\n${cerror(`${err.message}\n`)}`);
-      }
-      // console.log('\n---\n');
-      // throw err;
-    });
+  const runMigrations = await migrationUp();
+
+  await saveAllMigrations(Parse, runMigrations);
+
+  // TODO: Seed
+  if (shouldRunSeed) {
+    // Run seeders
+  }
+
+  console.log(`\n${csuccess('Successfully run migrations.\n')}`);
 };
